9aeee616352cc344ed028590812a391b1744bf57
flareon <flareon1337@gmail.com>
2019-11-17 02:42:43 -0600

    add basic win32 box

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[cfg(target_os = "windows")] mod win32;
+#[cfg(target_os = "windows")] pub use win32::show;
+
+#[test]
+fn test() {
+    show("Hello, world!", "boxy");
 }
